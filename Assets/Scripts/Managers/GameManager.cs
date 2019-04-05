7b5aa4af6f458a2f8ac49a49fe8a3fec09857c9a
jasmaa <jasonmaa3955@gmail.com>
2019-04-05 12:19:45 -0400

    Add vuforia, start ui

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager instance;
+
+	public int[] sceneCounts;
+
+	int comicNum = 2;
+	int sceneNum = 0;
+
+    void Start()
+    {
+		if(instance == null){
+			instance = this;
+		}
+		else{
+			Destroy(this.gameObject);
+		}
+    }
+
+	public void MoveScene(int n){
+		sceneNum = Mathf.Clamp(sceneNum + n, 0, sceneCounts[comicNum] - 1);
+		print(sceneNum);
+	}
+}
