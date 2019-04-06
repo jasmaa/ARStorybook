e6f6da2eb04b5a76f27d287ac88006c779060781
jasmaa <jasonmaa3955@gmail.com>
2019-04-06 00:19:46 -0400

    Start implementing comics

---
+++ Assets/Scripts/ComicScene.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ComicScene : MonoBehaviour, ITrackableEventHandler
+{
+
+	private TrackableBehaviour trackableBehaviour;
+	public bool isTracking = false;
+	public int currScene = 0;
+	public int maxScenes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		trackableBehaviour = GetComponent<TrackableBehaviour>();
+		if(trackableBehaviour){
+			trackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void MoveScene(int n){
+		currScene = Mathf.Clamp(currScene + n, 0, maxScenes);
+	}
+
+	public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus,TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			newStatus == TrackableBehaviour.Status.TRACKED ||
+			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED){
+
+			isTracking = true;
+		}
+		else{
+			isTracking = false;
+		}
+	}
+}
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -6,9 +6,8 @@ public class GameManager : MonoBehaviour
 {
 	public static GameManager instance;
 
-	public int[] sceneCounts;
-
-	int comicNum = 2;
+	List<ComicScene> comicScenes;
+	int comicNum = 0;
 	int sceneNum = 0;
 
     void Start()
@@ -19,10 +18,35 @@ public class GameManager : MonoBehaviour
 		else{
 			Destroy(this.gameObject);
 		}
+
+		// Init
+		comicScenes = new List<ComicScene>();
+
+		// Load all comics
+		foreach(Transform child in GameObject.Find("Comics").transform){
+			comicScenes.Add(child.GetComponent<ComicScene>());
+		}
     }
 
+	void Update(){
+		// Update comic num
+		comicNum = -1;
+		for(int i=0; i < comicScenes.Count; i++){
+			if(comicScenes[i].isTracking){
+				comicNum = i;
+				break;
+			}
+		}
+			
+		print(comicNum);
+	}
+
 	public void MoveScene(int n){
-		sceneNum = Mathf.Clamp(sceneNum + n, 0, sceneCounts[comicNum] - 1);
-		print(sceneNum);
+
+		if(comicNum < 0){
+			return;
+		}
+
+		comicScenes[comicNum].MoveScene(n);
 	}
 }
